d48f04d5deb3f0e5eacf25a8db715e7d0779d302
Арина В. Осипова <osipova.a.v@rd.mirea.ru>
2016-09-22 10:31:05 +0300

    fdjkgh

---
+++ math.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <math.h>
+
+using namespace std;
+
+void myfunc(int x) {
+cout << "My x: " << sqrt (x) << endl;
+}
+
+
+int main() {
+	
+	cout << "Hello world!" << endl;
+myfunc(100);
+	return 0;
+}
+
+
+
